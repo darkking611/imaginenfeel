c9ee43badbd2adae0217e5c26c08f8985535081e
Manoj Kumar <611manojkumar@gmail.com>
2020-03-28 01:12:56 +0530

    fs

--- myjs.js
+++ myjs.js
@@ -32,3 +32,19 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+function myAccFunc() {
+  var x = document.getElementById("demoAcc");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+document.getElementById("myBtn").click();
